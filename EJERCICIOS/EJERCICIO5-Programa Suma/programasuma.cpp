00be2df37dda52147d8a19a8454542ef58c98af8
Antonio Villatoro <anoniovillatoro8@gmail.com>
2020-07-08 20:54:29 -0600

    Agregando ejercicio 5 del programa suma

---
+++ EJERCICIOS/EJERCICIO5-Programa Suma/programasuma.cpp
@@ -0,0 +1,39 @@
+#include <iostream>
+using namespace std;
+
+int main(int argc, char const *argv[])
+{
+    //SE DECLARAN LAS VARIABLES
+    int a=0;
+    int b=0;
+    int resultado=0;
+
+    // INGRESO DE DATOS
+    
+    cout << "INGRESE EL VALOR DE A: ";
+    cout << endl;
+    cin >> a;
+    cout << endl;
+
+
+    cout << "INGRESE EL VALOR DE B:";
+    cout << endl;
+    cin >> b;
+    cout << endl;
+  
+    // PROCESO
+
+    resultado= a+b;
+
+    // SALIDA
+
+    cout <<"LA SUMA DE A + B  ES:  " << resultado;
+    cout << endl;
+    cout << endl;
+
+
+    // Comentario
+    
+    
+    return 0;
+}
