d700d028f93f6a150145e645ce5db6ad118891f1
Antonio Villatoro <anoniovillatoro8@gmail.com>
2020-07-12 20:02:14 -0600

    agregando operador logico and

---
+++ EJERCICIOS/EJERCICIO11-Operador-logico-and/operador-and.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+using namespace std;
+
+int main(int argc, char const *argv[])
+{
+    int a=5;
+    int b=10;
+    int c=7;
+
+    if (a > b && a > c)
+    {
+        cout << "A es el mayor   " << a;
+        cout << endl;
+    } 
+    if (b > a && b > c)
+    {
+       cout << "B es el mayor  " << b;
+       cout << endl;
+    }
+     if (c > a && c> b)
+     {
+       cout << "C es el mayor " << c;
+       cout << endl;
+       } 
+
+    return 0;
+}
