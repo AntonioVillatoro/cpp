09a6bb10ad5b33ed3e89f7d8ea962cf2f1401098
Antonio Villatoro <anoniovillatoro8@gmail.com>
2020-07-14 20:27:05 -0600

    comprobando if-else

--- EJERCICIOS/EJERCICIO14-Notas/notas.cpp
+++ EJERCICIOS/EJERCICIO14-Notas/notas.cpp
@@ -15,31 +15,31 @@ if (nota >= 95 && nota <= 100){
     if (nota >= 85 && nota <= 94){
      cout << "OBTUVISTE UNA B"; 
      cout << endl;  
-} else {
-    if (nota >= 75 && nota <= 84){
-     cout << "OBTUVISTE UNA C";
-     cout << endl;   
-} else {
-    if (nota >= 65 && nota <= 74){
-     cout << "OBTUVISTE UNA D"; 
-     cout << endl;  
-} else {
-    if (nota >= 60 && nota <= 64){
-     cout << "OBTUVISTE UNA E";
-     cout << endl;   
-} else {
-    if (nota >= 0 && nota < 60){
-     cout << "OBTUVISTE UNA F"; 
-     cout << endl;  
-} else {
-    cout << "INGRESE UNA NOTA DE 0-100";
-    cout << endl;
-}
-}
-}
-}
-}
-}
+    } else {
+        if (nota >= 75 && nota <= 84){
+        cout << "OBTUVISTE UNA C";
+        cout << endl;   
+        } else {
+            if (nota >= 65 && nota <= 74){
+            cout << "OBTUVISTE UNA D"; 
+            cout << endl;  
+            } else {
+                if (nota >= 60 && nota <= 64){
+                cout << "OBTUVISTE UNA E";
+                cout << endl;   
+                } else {
+                    if (nota >= 0 && nota < 60){
+                    cout << "OBTUVISTE UNA F"; 
+                    cout << endl;  
+                        } else {
+                        cout << "INGRESE UNA NOTA DE 0-100";
+                        cout << endl;
+                        } 
+                    }
+                } 
+           }     
+        }
+    }
 
     return 0;
 }
