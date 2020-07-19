c288584bd65c583eb2dd9b83d5e2d636f47d33d1
Antonio Villatoro <anoniovillatoro8@gmail.com>
2020-07-19 13:48:44 -0600

    agregando ejercicio 16 ciclo while

---
+++ EJERCICIOS/EJERCICIO16-Ciclos While/cicloswhile.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+using namespace std;
+
+int main(int argc, char const *argv[])
+{
+  int a=5;
+  int b=2;
+
+//CICLO INFINITO
+
+  while (a > b){
+      cout << "INGRESE EL VALOR DE A" << endl;
+      cin >> a;
+
+  }
+  
+cout << endl;
+cout << "SALISTE DEL CICLO" << endl;
+
+
+
+    return 0;
+}
