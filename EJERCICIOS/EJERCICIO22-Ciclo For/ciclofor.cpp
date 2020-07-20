1a08274e2d4fa470b743a18c69ca637c3f0d842c
Antonio Villatoro <anoniovillatoro8@gmail.com>
2020-07-19 18:08:58 -0600

    agregando ejercicio22 ciclo for

---
+++ EJERCICIOS/EJERCICIO22-Ciclo For/ciclofor.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main(int argc, char const *argv[])
+{
+   
+for (int indice = 0; indice <=10; indice ++)
+{
+    cout << "VALOR DE LA VARIABLE INDICE " << indice << endl;
+}
+
+    return 0;
+}
+
