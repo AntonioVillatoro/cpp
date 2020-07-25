7cd8b30889b7f7d126fa43c841102b6570149bbd
Antonio Villatoro <anoniovillatoro8@gmail.com>
2020-07-25 13:37:13 -0600

    agregando repaso 1

---
+++ EJERCICIOS/EJERCICIO26-REPASO1/Repaso1.cpp
@@ -0,0 +1,40 @@
+#include <iostream>
+using namespace std;
+
+int main(int argc, char const *argv[])
+{
+
+   for (int i = 0; i <= 10; i++)
+    {
+        cout << "INDICE:  " << i;
+        cout << endl;
+    }
+
+   /*    while (true)
+        {
+            cout << "DENTRO DE UN WHILE";
+            if (i == 5)
+            {
+                switch (i)
+                {
+                case 1:
+                    break;
+                default:
+                    break;
+                }
+            }
+        }
+    }*/
+
+cout << endl;
+cout << endl;
+
+int i=1;
+while (i<=10)
+{
+cout << "INDICE:  " << i;
+cout << endl;
+i++;
+}
+    return 0;
+}
