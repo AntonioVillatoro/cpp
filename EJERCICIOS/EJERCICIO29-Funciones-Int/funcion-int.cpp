7af3793833f6c9f782c701e98ed1609862c42d56
Antonio Villatoro <anoniovillatoro8@gmail.com>
2020-07-30 17:40:17 -0600

    agregando ejercicio 29 funciones-int

---
+++ EJERCICIOS/EJERCICIO29-Funciones-Int/funcion-int.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+using namespace std;
+
+int sumar (int a, int b){
+    return a+b;
+
+}
+
+int main(int argc, char const *argv[])
+{
+    system ("cls");
+    cout << sumar (5,7);
+    cout << endl;
+    cout << sumar (10,7);
+    cout << endl;
+    cout << sumar (8,7);
+    cout << endl;
+    cout << sumar (5,5);
+    cout << endl;
+    cout << sumar (15,15);
+    cout << endl;
+
+    return 0;
+}
