305e5a9ad1d38a2fb627c0e512b485a907d6b252
Antonio Villatoro <anoniovillatoro8@gmail.com>
2020-08-01 22:43:49 -0600

    agregando ejercicio 38 repaso 4

---
+++ EJERCICIOS/EJERCICIO38-REPASO4/main.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+using namespace std;
+
+int main(int argc, char const *argv[])
+{
+
+    system ("cls");
+
+
+    for (int  i = 5; i <= 100; i= i+5)
+    {
+    
+        cout << i << endl;
+        if (i==25){
+            break;
+        }
+
+    }
+
+    // cout << endl << endl << endl;
+    
+  
+
+    // int i=5;
+    // while (i <=100){
+    //     cout << i << endl;
+    //     i=i+5;
+    //     if (i > 25){
+    //         break;
+    //     }
+    // }
+
+
+
+    return 0;
+}
