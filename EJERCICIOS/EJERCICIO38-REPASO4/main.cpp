f6456f413b380abd455fb9a8a649263ff32fd6ef
Antonio Villatoro <anoniovillatoro8@gmail.com>
2020-08-02 10:00:45 -0600

    terminando de agregar ejercicio 38 repaso 4

--- EJERCICIOS/EJERCICIO38-REPASO4/main.cpp
+++ EJERCICIOS/EJERCICIO38-REPASO4/main.cpp
@@ -4,31 +4,80 @@ using namespace std;
 int main(int argc, char const *argv[])
 {
 
-    system ("cls");
+    // system("cls");
 
+    // for (int i = 5; i <= 100; i = i + 5)
+    // {
 
-    for (int  i = 5; i <= 100; i= i+5)
-    {
-    
-        cout << i << endl;
-        if (i==25){
-            break;
-        }
+    //     cout << i << endl;
 
-    }
+    //     for (int j = 0; j <= 5; j++)
+    //     {
+
+    //         cout << j << endl;
+    //     }
+
+    //     for (int k = 0; k < 3; k++)
+    //     {
+    //         cout << k << endl;
+
+    //         if (i == 25)
+    //         {
+    //             break;
+    //         }
+    //     }
+
+    //     if (i == 25)
+    //     {
+    //         break;
+    //     }
+
+    //     if (i == 25)
+    //     {
+    //         break;
+    //     }
+    // }
 
     // cout << endl << endl << endl;
-    
-  
 
     // int i=5;
     // while (i <=100){
     //     cout << i << endl;
     //     i=i+5;
+    // int j=0;
+    // while (j=5){
+    // break;
+    //  }
+    // j++;
     //     if (i > 25){
     //         break;
     //     }
     // }
+
+// int i=5;
+//     do {
+//         cout << i << endl;
+//         i--;
+//     } while (i==-1);
+
+// int j=5;
+// while (j==-1);
+//    {
+//         cout << j << endl;
+//         j--;
+//     } while (j==-1);
+
+
+int nota=69;
+bool aprobo = (nota > 69 && nota <=75) || nota ==100;
+
+if (nota > 69){
+    aprobo=true;
+} else {
+    aprobo=false;
+}
+cout << aprobo;
+
 
 
 
