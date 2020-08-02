c2976c9b529497f265420f59548479b9e5db35d2
Antonio Villatoro <anoniovillatoro8@gmail.com>
2020-08-02 12:29:42 -0600

    agregando ejercicio 42

---
+++ EJERCICIOS/EJERCICIO42-Punto-de-Venta III/menu.cpp
@@ -0,0 +1,37 @@
+
+#include <iostream>
+using namespace std;
+
+extern void productos(int opcion);
+
+void menu()
+{
+    int opcion = 0;
+
+    while (true)
+    {
+        system("cls");
+        cout << "****";
+        cout << "MENU";
+        cout << "****";
+
+        cout << endl;
+        cout << endl;
+        cout << endl;
+
+        cout << "1-BEBIDAS CALIENTES" << endl;
+        cout << "2-BEBIDAS FRIAS" << endl;
+        cout << "3-REPOSTERIA" << endl;
+         cout << "4-IMPRIMIR FACTURAS" << endl;
+        cout << "0-SALIR" << endl;
+
+        cin >> opcion;
+
+        if (opcion == 0)
+        {
+            break;
+        }
+
+        productos(opcion);
+    }
+}
