fdf318ad2f82ec2126ad89522f3f04f4d2d702b2
Antonio Villatoro <anoniovillatoro8@gmail.com>
2020-08-07 10:38:24 -0600

    agregando ejercicio 45 ejercicio H

---
+++ EJERCICIOS/EJERCICIO45-Ejercicio-H/main.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include "calculadora.h"
+
+using namespace std;
+
+
+int main(int argc, char const *argv[])
+{
+    cout << endl;
+    cout << sumar (5, 7);
+    cout << endl;
+    cout << endl;
+
+    return 0;
+}
+
