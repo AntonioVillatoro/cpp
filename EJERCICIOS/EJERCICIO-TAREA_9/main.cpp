f76d8ec0f99213c4d8e922987d862b7d8f683924
Antonio Villatoro <anoniovillatoro8@gmail.com>
2020-08-09 21:43:39 -0600

    agregando TAREA 9

--- EJERCICIOS/EJERCICIO-TAREA_9/main.cpp
+++ EJERCICIOS/EJERCICIO-TAREA_9/main.cpp
@@ -15,24 +15,31 @@ int main(int argc, char const *argv[])
 
 while (respuesta== 'S' || respuesta== 's')
 {
-        //GENERAMOS LOS DADOS DEL 1 AL 6
+     //GENERAMOS LOS DADOS DEL 1 AL 6
+    cout << "BIENVENIDO A JUGAR DADOS" << endl;
+    cout << "SUERTE!" << endl;
     cout << "DESEA JUGAR DADOS: S/N" << endl;
     cin >> respuesta;
 
-    if (respuesta=='n' || respuesta== 'N'){
+
+    if (respuesta=='n' || respuesta== 'N')
+    {
         break;
     }
-
+     
+   
+    
     primerDado= rand ()%6+1;
     segundoDado= rand ()%6+1;
 
     //JUGAMOS
-
+    cout << endl << endl;
     cout << "PRIMER DADO : " << primerDado << endl;
     cout << "SEGUNDO DADO:  " << segundoDado << endl;
+    cout << endl << endl;
+
   
 }
-cout << endl;
 cout << endl;
 cout << "SALIO DE JUGAR DADOS" << endl;
 
