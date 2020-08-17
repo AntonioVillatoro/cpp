46e0a9c0c56e24c33ccfd3cc6ccac6efa3d2591f
Antonio Villatoro <anoniovillatoro8@gmail.com>
2020-08-17 07:45:54 -0600

    agregando ejercicio 57

--- EJERCICIOS/EJERCICIO57/main.cpp
+++ EJERCICIOS/EJERCICIO57/main.cpp
@@ -3,16 +3,15 @@ using namespace std;
 
 int main(int argc, char const *argv[])
 {
-    string nombre [3][2];
+    
 
-    nombre [0][0] ="JUAN" ;
-    nombre [0][1]="PEREZ";
+    string nombre [3][2]=
+{
+   {"JUAN" , "PEREZ"},
+   {"MARIA", "MARTINEZ"},
+   {"PEDRO", "JIMENEZ"},
 
-    nombre [1][0]="MARIA";
-    nombre [1][1]="MARTINEZ";
-
-    nombre [2][0]="PEDRO";
-    nombre [2][1]="JIMENEZ";
+};
 
     for (int i = 0; i < 3; i++)
     {
