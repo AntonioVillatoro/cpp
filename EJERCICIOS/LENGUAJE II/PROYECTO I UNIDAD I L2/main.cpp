0e66c576294fe744d255e063e221215c50412d28
Antonio Villatoro <anoniovillatoro8@gmail.com>
2020-10-03 12:39:41 -0600

    agregando codigo proyecto 1  L2

---
+++ EJERCICIOS/LENGUAJE II/PROYECTO I UNIDAD I L2/main.cpp
@@ -0,0 +1,43 @@
+#include <iostream>
+#include "startShip.h"
+#include "snake.h"
+
+using namespace std;
+
+int main(int argc, char const *argv[])
+{
+
+    int opcion = 0;
+
+    while (true)
+    {
+        system("cls");
+        cout << "***********" << endl;
+        cout << " BIENVENIDO " << endl;
+        cout << "***********" << endl << endl;
+        cout << "***************" << endl;
+        cout << " MENU DE JUEGO " << endl;
+         cout << "**************" << endl << endl;
+        cout << "SELECCIONE UN JUEGO: " << endl;
+        cout << "1- STARSHIP" << endl;
+        cout << "2-SNAKE" << endl;
+        cout << "INGRESE LA OPCION PARA JUGAR:" << endl;
+        cin >> opcion;
+
+        switch (opcion)
+        {
+        case 1:
+            system("cls");
+            starShip();
+            break;
+        case 2:
+            system("cls");
+            snake();
+            break;
+        default:
+            break;
+        }
+    }
+
+    return 0;
+}
